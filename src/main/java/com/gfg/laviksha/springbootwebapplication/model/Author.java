4679d8252b3fce156c9c09d85dad6806c228e7fd
laviksha <lavikshasoni@gmail.com>
2019-06-09 13:06:13 +0530

    book and author model added

---
+++ src/main/java/com/gfg/laviksha/springbootwebapplication/model/Author.java
@@ -0,0 +1,26 @@
+package com.gfg.laviksha.springbootwebapplication.model;
+
+public class Author {
+    private String firstName,lastName;
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
