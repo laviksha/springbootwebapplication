9cebe18975a40d846ff20c07234f762e027d4b04
laviksha <lavikshasoni@gmail.com>
2019-06-09 13:32:05 +0530

    ENtity addedd

--- src/main/java/com/gfg/laviksha/springbootwebapplication/model/Author.java
+++ src/main/java/com/gfg/laviksha/springbootwebapplication/model/Author.java
@@ -1,11 +1,25 @@
 package com.gfg.laviksha.springbootwebapplication.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+    @Entity  //jpa will make it entity
 public class Author {
+    @Id
+    private long id;
     private String firstName,lastName;
 
     public Author(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
--- src/main/java/com/gfg/laviksha/springbootwebapplication/model/Book.java
+++ src/main/java/com/gfg/laviksha/springbootwebapplication/model/Book.java
@@ -1,12 +1,26 @@
 package com.gfg.laviksha.springbootwebapplication.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity     //jpa will make it entity
 public class Book {
+    @Id
+    private long id;
     private String title;
     private String isbn;
 
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 
     public String getTitle() {
